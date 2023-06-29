4d440878eb0ef5ec947c0451d8b7cac73b317375
kutlo2019 <kutlopopo@rocketmail.com>
2023-06-29 21:44:38 +0200

    add button to adjust screen size, refactor function to create grid

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-// Create 16 x 16 square divs and add to .grid
+function createGrid(grid, size) {
+    grid.innerHTML = "";
+    for (let i = 0; i < size * size; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('.grid-square');
+        gridSquare.addEventListener('mouseover', e => {
+            e.target.style.backgroundColor = 'aqua';
+        });
+        grid.appendChild(gridSquare)
+    }
+}
+
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < 256; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('.grid-square');
-    gridSquare.addEventListener('mouseover', e => {
-        e.target.style.backgroundColor = 'aqua';
-    });
-    grid.appendChild(gridSquare)
-}
+createGrid(grid, 16);
+
+const screenSizer = document.getElementById('screen-size');
+screenSizer.addEventListener('click', e => {
+    let size = prompt("Please enter the desired screen size (less that 100 width)");
+    size = parseInt(size);
+    if (size > 100) {
+        size = 100;
+    }
+
+});
