5c1315183adc1a492ef35d9735399ac227b93d43
kutlo2019 <kutlopopo@rocketmail.com>
2023-06-29 21:15:24 +0200

    leave a color trail on grid on mouse over

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const grid = document.querySelector('.grid');
 
 for (let i = 0; i < 256; i++) {
     const gridSquare = document.createElement('div');
+    gridSquare.classList.add('.grid-square');
+    gridSquare.addEventListener('mouseover', e => {
+        e.target.style.backgroundColor = 'aqua';
+    });
     grid.appendChild(gridSquare)
 }
